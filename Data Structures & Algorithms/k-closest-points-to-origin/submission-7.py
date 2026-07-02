5a980e80fbb4b649985c3d2b98e4201fbfe92892
胡雨軒 Петр <3686502+piotr-yuxuan@users.noreply.github.com>
2026-07-02 11:11:29 +0100

    Add: k-closest-points-to-origin - submission-7

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-7.py
@@ -0,0 +1,33 @@
+from functools import cache
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        def metric(p) -> int:
+            return p[0]**2 + p[1]**2
+
+        # Stateful. Inclusive start, exclusive end.
+        def partition(s, e):
+            left = s
+            pivot = e-1
+
+            m = metric(points[pivot])
+            for i in range(s, pivot):
+                if metric(points[i]) < m:
+                    points[i], points[left] = points[left], points[i]
+                    left += 1
+            points[pivot], points[left] = points[left], points[pivot]
+
+            return left
+        
+        s, e = 0, len(points)
+        pivot = -1
+
+        while pivot != k-1:
+            pivot = partition(s, e)
+            if pivot < k:
+                s = pivot+1
+            else:
+                e = pivot
+        
+        return points[:k]
