d8d3c32d9649e2777cdd04fe02a3b596b759c219
胡雨軒 Петр <3686502+piotr-yuxuan@users.noreply.github.com>
2026-07-10 15:19:27 +0200

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+
+class WordDictionary:
+
+    wildcard = '.'
+
+    def __init__(self):
+        self.children = defaultdict(WordDictionary)
+        self.words = 0
+        
+
+    def addWord(self, word: str) -> None:
+        c = word[:1]
+        if not c:
+            self.words += 1
+        else:
+            self.children[c].addWord(word[1:])
+            if WordDictionary.wildcard != c:
+                self.children[WordDictionary.wildcard].addWord(word[1:])
+        
+
+    def search(self, word: str) -> bool:
+        c = word[:1]
+        if not c:
+            return 0 < self.words
+        elif c in self.children:
+            return self.children[c].search(word[1:])
+        else:
+            return False
