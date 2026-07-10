5bdc30e55ca14d53486e229b98adf0c90fd88770
胡雨軒 Петр <3686502+piotr-yuxuan@users.noreply.github.com>
2026-07-10 12:05:13 +0200

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+class PrefixTree:
+
+    def __init__(self):
+        self.children = defaultdict(PrefixTree)
+        self.word = 0
+        
+
+    def insert(self, word: str) -> None:
+        c = word[:1]
+        if not c:
+            self.word += 1
+        else:
+            self.children[c].insert(word[1:])
+
+
+    def _search(self, word: str) -> Optional[PrefixTree]:
+        c = word[:1]
+        if not c:
+            return self
+        elif c in self.children:
+            return self.children[c]._search(word[1:])
+        else:
+            return None
+
+
+    def search(self, word: str) -> bool:
+        leaf = self._search(word)
+        return bool(leaf and 0 < leaf.word)
+
+
+    def startsWith(self, prefix: str) -> bool:
+        leaf = self._search(prefix)
+        return bool(leaf)
+        
