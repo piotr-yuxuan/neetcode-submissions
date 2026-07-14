2e3dbe18244b86b962c3c6bc08407a43a4001741
胡雨軒 Петр <3686502+piotr-yuxuan@users.noreply.github.com>
2026-07-14 17:41:56 +0200

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        permutations = [[]]
+        for n in nums:
+            current = []
+            for p in permutations:
+                for i in range(len(p)+1):
+                    q = list(p)
+                    q.insert(i, n)
+                    current.append(q)
+            permutations = current
+        return permutations
+                
+
+        
+    def _permute(self, nums: List[int]) -> List[List[int]]:
+        if 0 == len(nums):
+            return []
+
+        def dfs(i):
+            if len(nums) == i:
+                return [[]]
+
+            permutations = []
+            for p in dfs(i + 1):
+                for j in range(len(p) + 1):
+                    q = list(p)
+                    q.insert(j, nums[i])
+                    permutations.append(q)
+
+            return permutations
+
+        return dfs(0)
