96a65d265a399766deb7c84015bc4cf2b73d3ea0
胡雨軒 Петр <3686502+piotr-yuxuan@users.noreply.github.com>
2026-07-17 17:14:28 +0200

    Add: course-schedule-iv - submission-9

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-9.py
@@ -0,0 +1,52 @@
+from collections import deque
+import functools
+
+
+class Solution:
+    def checkIfPrerequisite(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        queries: List[List[int]],
+    ) -> List[bool]:
+        adjacency = [[float("+inf") for j in range(numCourses)] for i in range(numCourses)]
+
+        for n1, n2 in prerequisites:
+            adjacency[n1][n2] = 1
+
+        for i in range(numCourses):
+            for j in range(numCourses):
+                for k in range(numCourses):
+                    adjacency[i][j] = min(adjacency[i][j], adjacency[i][k] + adjacency[k][j])
+
+        return [isinstance(adjacency[u][v], int) for u, v in queries]
+
+    def _checkIfPrerequisite(
+        self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]
+    ) -> List[bool]:
+        flipped = {i: set() for i in range(numCourses)}
+        for n1, n2 in prerequisites:
+            flipped[n2].add(n1)
+
+        path = set()
+        ancestors = {i: set() for i in range(numCourses)}
+
+        def dfs(node):
+            for x in path:
+                ancestors[x].add(node)
+            if ancestors[node]:
+                for x in path:
+                    for y in ancestors[node]:
+                        ancestors[x].add(y)
+                return
+            if node in path:
+                return
+            path.add(node)
+            for child in flipped[node]:
+                dfs(child)
+            path.remove(node)
+
+        for node in flipped.keys():
+            dfs(node)
+
+        return [u in ancestors[v] for u, v in queries]
