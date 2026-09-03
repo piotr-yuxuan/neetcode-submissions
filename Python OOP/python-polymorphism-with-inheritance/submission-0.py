eff16454c0b30afe6f0231b938e59901c26d896b
胡雨軒 Петр <3686502+piotr-yuxuan@users.noreply.github.com>
2026-09-03 20:11:23 +0100

    Add: python-polymorphism-with-inheritance - submission-0

---
+++ Python OOP/python-polymorphism-with-inheritance/submission-0.py
@@ -0,0 +1,35 @@
+from typing import override
+
+
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def make_sound(self) -> None:
+        print("Animal is making a sound")
+
+
+# TODO: Create the Dog and Cat classes with make_sound method
+class Dog(Animal):
+    @override
+    def make_sound(self) -> None:
+        print(f"{self.name} says: Woof!")
+
+class Cat(Animal):
+    @override
+    def make_sound(self) -> None:
+        print(f"{self.name} says: Meow!")
+
+# TODO: Create a common interface that takes any object of type Animal (or its subclasses) and calls their make_sound method
+def my_function(animal: Animal) -> None:
+    animal.make_sound()
+
+# Do not change the code below
+animal = Animal("Rabbit")
+animal.make_sound()
+
+animal = Dog("Buddy")
+animal.make_sound()
+
+animal = Cat("Whiskers")
+animal.make_sound()
