99c172b2c524ce53bc6c784d476e4912ef6c7e4c
胡雨軒 Петр <3686502+piotr-yuxuan@users.noreply.github.com>
2026-09-03 21:20:43 +0100

    Add: python-interface - submission-2

---
+++ Python OOP/python-interface/submission-2.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from typing import override, final
+
+# TODO: Create a Superhero interface
+class Superhero(ABC):
+    @abstractmethod
+    def fly(self) -> str:
+        pass
+    
+    @abstractmethod
+    def use_power(self) -> str:
+        pass
+
+@final
+class Superman(Superhero):
+    @override
+    def fly(self) -> str:
+        return "Up, up and away!"
+    
+    @override
+    def use_power(self) -> str:
+        return "Using heat vision"
+
+@final
+class WonderWoman(Superhero):
+    @override
+    def fly(self) -> str:
+        return "Soaring through the clouds!"
+    
+    @override
+    def use_power(self) -> str:
+        return "Using lasso of truth"
+
+
+# Don't modify the code below
+superman = Superman()
+wonder_woman = WonderWoman()
+
+print(isinstance(superman, Superhero)) 
+print(isinstance(wonder_woman, Superhero))
